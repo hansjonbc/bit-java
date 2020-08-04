3961d06779146ad3110ce1333fa5178ebd113244
Han Ho Jun <hansjonbc@naver.com>
2020-08-04 11:04:07 +0900

    첫 번째 커밋

---
+++ JavaEx/src/com/javaex/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.javaex.basic;
+/*
+ * 화면에 Hello, World라 출력하자 단축키 alt + shift + x + j
+ */
+public class HelloWorld {
+
+	//직접 실행을 위해서는 main 메서드가 있어야 한다.
+	public static void main(String[] args) {
+		//출력 코드
+		System.out.println("Hello, World");
+	}
+}
